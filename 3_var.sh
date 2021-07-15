57f90363180369f8eb17c4daba44847297e43b58
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-15 15:25:23 +0530

    PRACTICE WITH VARIEBLES with date

--- 3_var.sh
+++ 3_var.sh
@@ -7,3 +7,6 @@ echo Welcome,Today date is $DATE
 ## command subs
 NO_OF_USERS=$(who | wc -l)
 echo Number of users = $NO_OF_USERS
+##Date with command substitution
+DATE=(date +%F)
+echo Welcome Todays date is $DATE
