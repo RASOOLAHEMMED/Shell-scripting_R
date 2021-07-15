d8eef71b5edd03dd403da8fe57a895d2a40b14c9
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-15 16:47:25 +0530

    FUNCTION PRACTICE with declare variable with arguments

--- 6_function.sh
+++ 6_function.sh
@@ -5,10 +5,13 @@ sample() {
   echo Hello , i am a simple function
   echo value of a = ${a}
   b=200
+  echo first argument in function=$1
 }
 
+## main program
 ## call the fucntion
 
 a=100
 sample
 echo value of b = ${b}
+echo first argument in function=$1
