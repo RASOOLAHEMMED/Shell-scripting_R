b28afc4d43ed7e625db4eb792152dbf92c9ffc26
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-15 19:00:30 +0530

    frontend test

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo -e "installing Nginx\t\t...\t\e[32mdone\e[0m"
 yum install nginx -y
+###1. Out from command shoud not be displayed
+###2. Validate the command is successfull or not
 
 echo "enabling Nginx"
 systemctl enable nginx
