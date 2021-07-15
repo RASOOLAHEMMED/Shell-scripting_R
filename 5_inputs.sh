0372a71aab2eccf61488336434cb1747aa0ce64b
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-15 16:25:42 +0530

    input practice

--- 5_inputs.sh
+++ 5_inputs.sh
@@ -3,5 +3,12 @@
 ## inputs can be loaded using special variables
 ## 0-n,*,@,#
 
-## in script if you want access script name then
+## in script if you want access script name (bash 5_inputs.sh xyz 123) then
 echo $0
+##$1 is the first argument and $n is the nth argument
+echo $1
+## Pull all the passed values (bash 5_inputs.sh xyz 123)
+echo *
+echo @
+## number of values passed is
+echo $#
