85d98249b7364961b63ab2c53be16be099b4b598
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-15 18:44:48 +0530

    frontend test

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,7 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 echo "installing Nginx\t\t...\t\e[32mdone\e[0m"
 yum install nginx -y
+
 echo "enabling Nginx"
 systemctl enable nginx
+
 echo "starting Nginx"
 systemctl start enginx
