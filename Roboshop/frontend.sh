5a47858ae1ecfcb7fa1cd4efca322dcb0cb9ca27
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-16 17:45:21 +0530

    validating exit status with if condition

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -13,7 +13,7 @@ fi
 ###echo $?   ### to print the exist status
 ###3. need to validate script running with root user or not
 
-echo -n -e "enabling Nginx\t\t... "
+echo -n -e "enabling Nginx\t\t\t... "
 systemctl enable nginx &>>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32mDone\e[0m"
@@ -22,7 +22,7 @@ else
 fi
 ###echo $?
 
-echo "starting Nginx\t\t... "
+echo -n -e "starting Nginx\t\t\t... "
 systemctl start nginx &>>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32mDone\e[0m"
