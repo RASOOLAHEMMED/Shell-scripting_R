21dc16d84e41c59febfea1017f8361fc0ff5f658
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-16 15:47:16 +0530

    frontend test with redirectionlab

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-LOG=/tmp/roboshop.log
-
 echo -e "installing Nginx\t\t...\t\e[32mdone\e[0m"
-yum install nginx -y >$LOG
+yum install nginx -y
 ###1. Out from command shoud not be displayed
 ###2. Validate the command is successfull or not
 
 echo "enabling Nginx"
-systemctl enable nginx >$LOG
+systemctl enable nginx
 
 echo "starting Nginx"
-systemctl start nginx >$LOG
+systemctl start nginx
