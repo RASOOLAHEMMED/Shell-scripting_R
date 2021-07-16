a4c18e83a25d2bc5aed4c0b9c5950fd8b5f41f80
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-16 17:55:10 +0530

    Validate with function to reduce rededency of if condition

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -2,34 +2,28 @@
 LOG=/tmp/roboshop.log
 rm -f $LOG   ###to keep latest log
 
-echo -n -e "installing Nginx\t\t..."
-yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]; then
+STATUS_CHECK() {
+  if [ $1 -eq 0 ]; then
   echo -e "\e[32mDone\e[0m"
 else
     echo -e "\e[31mFail\e[0m"
     exit 1
 fi
+}
+
+echo -n -e "installing Nginx\t\t..."
+yum install nginx -y &>>$LOG
+STATUS_CHECK $?
 
 ###echo $?   ### to print the exist status
 ###3. need to validate script running with root user or not
 
 echo -n -e "enabling Nginx\t\t\t..."
 systemctl enable nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mDone\e[0m"
-else
-    echo -e "\e[31mFail\e[0m"
-    exit 1   ### if fail need to come out with script
-fi
+STATUS_CHECK $?
 ###echo $?
 
 echo -n -e "starting Nginx\t\t\t..."
 systemctl start nginx &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mDone\e[0m"
-else
-    echo -e "\e[31mFail\e[0m"
-    exit 1
-fi
+STATUS_CHECK $?
 ###echo $?
