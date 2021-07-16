d72627e778edcc8fbf0e8805119a7876f8cf4bdd
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-16 17:00:36 +0530

    to print the exit status

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -3,10 +3,14 @@ LOG=/tmp/roboshop.log
 rm -f $LOG   ###to keep latest log
 echo -e "installing Nginx\t\t...\t\e[32mdone\e[0m"
 yum install nginx -y &>>$LOG
+echo $?   ### to print the exist status
 ###1. Out from command shoud not be displayed
 ###2. Validate the command is successfull or not
+###3. need to validate script running with root user or not
 
 echo "enabling Nginx"
 systemctl enable nginx &>>$LOG
+echo $?
 echo "starting Nginx"
 systemctl start nginx &>>$LOG
+echo $?
