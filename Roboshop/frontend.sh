06f96e85b65d8a81d1d9aca91b211d185e16f493
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-16 17:33:28 +0530

    validating exit status with if condition

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 LOG=/tmp/roboshop.log
 rm -f $LOG   ###to keep latest log
-echo -e "installing Nginx\t\t...\t\e[32mdone\e[0m"
+echo -e "installing Nginx\t\t\t... "
 yum install nginx -y &>>$LOG
-echo $?   ### to print the exist status
-###1. Out from command shoud not be displayed
-###2. Validate the command is successfull or not
+if[$? -eq 0]; then
+  echo Done
+else
+    echo Fail
+fi
+
+###echo $?   ### to print the exist status
 ###3. need to validate script running with root user or not
 
 echo "enabling Nginx"
