2b4f09b27b8dec5ce59f439d3485712817e5e55e
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-16 18:05:18 +0530

    Validate with function to reduce rededency of if condition

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -22,12 +22,12 @@ STATUS_CHECK $?
 ###echo $?   ### to print the exist status
 ###3. need to validate script running with root user or not
 
-print "enabling Nginx"
+print "enabling Nginx\t"
 systemctl enable nginx &>>$LOG
 STATUS_CHECK $?
 ###echo $?
 
-print "starting Nginx"
+print "starting Nginx\t"
 systemctl start nginx &>>$LOG
 STATUS_CHECK $?
 ###echo $?
