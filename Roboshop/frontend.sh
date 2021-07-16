84c00881598c01e5677cb31f8eb0e8182933d44d
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-16 15:45:57 +0530

    frontend test with redirectionlab

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -3,7 +3,7 @@
 LOG=/tmp/roboshop.log
 
 echo -e "installing Nginx\t\t...\t\e[32mdone\e[0m"
-yum install nginx -y >$LOG      ###/tmp/roboshop.log
+yum install nginx -y >$LOG
 ###1. Out from command shoud not be displayed
 ###2. Validate the command is successfull or not
 
