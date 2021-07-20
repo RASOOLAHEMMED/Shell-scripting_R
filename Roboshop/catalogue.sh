ea18af43281c4aed6dd46426968fe3c68a02c133
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-20 17:08:19 +0530

    practice with catalogue

--- Roboshop/catalogue.sh
+++ Roboshop/catalogue.sh
@@ -12,12 +12,13 @@ useradd roboshop &>>$LOG
 fi
 STATUS_CHECK $?
 
-#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#$ cd /home/roboshop
-#$ unzip /tmp/catalogue.zip
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
+print "Download application"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+STATUS_CHECK $?
+
+print "Extract Downloaded code"
+cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install
+STATUS_CHECK $?
 
 # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 # systemctl daemon-reload
