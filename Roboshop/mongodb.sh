09d10a219ac7db11a3b5cb26e37aed94d0f58df4
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-20 15:40:14 +0530

    practice with mongodb

---
+++ Roboshop/mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source common.sh
+
+print "Settingup Repository\t"
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
+STATUS_CHECK $?
+
+
+
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+#Config file: /etc/mongod.conf
+
+# systemctl restart mongod
+Every Database needs the schema to be loaded for the application to work.
+Download the schema and load it.
+
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
