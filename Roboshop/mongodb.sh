3f631f7a2b50106ec661bdec1036f79dc5a7bc0e
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-20 16:22:37 +0530

    practice with mongodb

--- Roboshop/mongodb.sh
+++ Roboshop/mongodb.sh
@@ -28,6 +28,7 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongo
 STATUS_CHECK $?
 
 print "Load schema\t\t\t"
-cd /tmp && unzip -o mongodb.zip && cd mongodb-main && mongo < catalogue.js && mongo < users.js
+cd /tmp && unzip -o mongodb.zip &>>$LOG && cd mongodb-main && mongo < catalogue.js &>>$LOG && mongo < users.js &>>$LOG
 STATUS_CHECK $?
+
 
