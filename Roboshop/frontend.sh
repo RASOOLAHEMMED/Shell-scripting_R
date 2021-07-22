1dcd8b09a9cc0c3494211c95b13d4b5a179de251
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-22 10:59:23 +0530

    configured user application

--- Roboshop/frontend.sh
+++ Roboshop/frontend.sh
@@ -22,7 +22,7 @@ STATUS_CHECK $?
 ###echo $?   ### to print the exist status
 
 print "Update roboshop config\t"
-sed -i -e '/catalogue/ s/localhost/catalogue-frontend.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+sed -i -e '/catalogue/ s/localhost/catalogue-frontend.roboshop.internal/' -e '/user/ s/localhost/user.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
 STATUS_CHECK $?
 
 print "enabling Nginx\t\t"
