9ac13b19056095a51f44219ef327e8d9c3f1a238
RASOOLAHEMMED <rahemmed.eee@gmail.com>
2021-07-22 13:28:12 +0530

    configured user application

--- Roboshop/common.sh
+++ Roboshop/common.sh
@@ -57,9 +57,7 @@ STATUS_CHECK $?
 
 
 print "setup systemd file\t"
-sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e
-"s/CATALOGUE_ENDPOINT/catalogue-frontend.roboshop.internal/" /home/roboshop/${component}/systemd.service && mv /home/roboshop/${component}/systemd.service
-/etc/systemd/system/${component}.service
+sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" /home/roboshop/${component}/systemd.service && mv /home/roboshop/${component}/systemd.service /etc/systemd/system/${component}.service
 STATUS_CHECK $?
 
 print "Start ${component} service\t"
